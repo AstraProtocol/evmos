b23f72e867674616eb92c993cf32fb6e77be5517
Khanh Hoa <49144992+hoanguyenkh@users.noreply.github.com>
2022-10-24 11:41:17 +0700

    deps: bump cosmos sdk to v0.45.9, ibc-go to v3.2.0 (#6)

--- testutil/network/util.go
+++ testutil/network/util.go
@@ -131,7 +131,6 @@ func startInProcess(cfg Config, val *Validator) error {
 		tmRPCAddr := fmt.Sprintf("tcp://%s", val.AppConfig.GRPC.Address)
 
 		val.jsonrpc, val.jsonrpcDone, err = server.StartJSONRPC(val.Ctx, val.ClientCtx, tmRPCAddr, tmEndpoint, val.AppConfig, nil)
-
 		if err != nil {
 			return err
 		}
